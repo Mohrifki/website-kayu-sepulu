eaf92a27bf696f7318376c736ddeb243649ce80f
MOH RIFKI WIDIYANTO <mohrifki15615@gail.com>
2021-04-29 21:33:16 +0700

    project wtk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data";
+import "./App.css";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import ProductScreen from "./Screens/ProductScreen";
+import HomeScreen from "./Screens/HomeScreen";
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+        <Link to = "/" >Kasepu</Link>
+          </div>
+          <div className="header-links">
+            <a href="Keranjang.html">Chart </a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Category</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul>
+            <li>
+              <a href="index.html">Bengkirai</a>
+            </li>
+            <li>
+              <a href="index.html">Galam</a>
+            </li>
+            <li>
+              <a href="index.html">Meranti</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+            
+          </div>
+        </main>
+        <footer className="footer">Bismillah Bisa.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
