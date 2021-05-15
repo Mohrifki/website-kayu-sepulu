c857c80ad9fd791d86612cbd49165ef2b924b8ff
MOH RIFKI WIDIYANTO <mohrifki15615@gail.com>
2021-05-15 21:02:03 +0700

    redux to detail(advanced)

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,27 +1,43 @@
-import React from "react";
-import data from '../data';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+// import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { lisProducts } from "../actions/productActions";
 
 function HomeScreen(props) {
-  return (
-    <ul className="products">
-      {data.products.map((product) => 
-        <li>
-          <div className="product">
-            <img className="product-image" src={product.image} alt="product" />
-            <div className="product-name">
-              <Link to={'/product/' + product._id}>{product.name}</Link>
-            </div>
-            <div className="prouct-brand">{product.brand}</div>
-            <div className="product-price">{product.price}</div>
-            <div className="product-rating">
-              {product.rating} Stars ({product.numReviews})
-            </div>
-          </div>
-        </li>
-      )}
+  const productList = useSelector(state => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(lisProducts());
+
+    return () => {
+      //
+    };
+  }, []);
+
+  return loading ? <div>Loading...</div>: 
+    error ? <div>{error}</div>:
+      <ul className="products">
+          {
+            products.map((product) => (
+            <li key={product._id}>
+              <div className="product">
+                <img className="product-image" src={product.image} alt="product" />
+                <div className="product-name">
+                  <Link to={"/product/" + product._id}>{product.name}</Link>
+                </div>
+                <div className="prouct-brand">{product.brand}</div>
+                <div className="product-price">{product.price}</div>
+                <div className="product-rating">
+                  {product.rating} Stars ({product.numReviews})
+                </div>
+              </div>
+            </li>
+            ))
+          }
     </ul>
-  );
-}
+};
 
 export default HomeScreen;
